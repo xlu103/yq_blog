d031f680cfd35e699046d4946b14cfeb98bd1ff2
xlu <xlu103@126.com>
2024-10-07 09:26:08 +0000

    更新语雀文档

--- yuque_doc.js
+++ yuque_doc.js
@@ -1,14 +1,5 @@
 const articleList = [
   {
-    "title": "《复盘》｜学会思考自己的思考 · 语雀",
-    "description": "任何不以改变为目的的复盘都是假复盘。",
-    "url": "https://www.yuque.com/xlu103/rvt9mr/am9x60rvhwzuqrbg",
-    "image": "https://cdn.nlark.com/yuque/0/2024/png/34813220/1725891325031-0e80573e-0cf6-4a71-9368-222f8249d12d.png",
-    "create_time": "2024-09-09 15:55:36",
-    "update_time": "2024-09-09 22:15:36",
-    "link": "https://www.yuque.com/xlu103/rvt9mr/am9x60rvhwzuqrbg"
-  },
-  {
     "title": "《微习惯》简单到不可能失败的自我管理法则 · 语雀",
     "description": "只要往小处想，你就会成为自己生活的主宰。",
     "url": "https://www.yuque.com/xlu103/rvt9mr/tclmoq3n4v3pwd4v",
@@ -18,6 +9,24 @@ const articleList = [
     "link": "https://www.yuque.com/xlu103/rvt9mr/tclmoq3n4v3pwd4v"
   },
   {
+    "title": "《复盘》｜学会思考自己的思考 · 语雀",
+    "description": "任何不以改变为目的的复盘都是假复盘。",
+    "url": "https://www.yuque.com/xlu103/rvt9mr/am9x60rvhwzuqrbg",
+    "image": "https://cdn.nlark.com/yuque/0/2024/png/34813220/1725891325031-0e80573e-0cf6-4a71-9368-222f8249d12d.png",
+    "create_time": "2024-09-09 15:55:36",
+    "update_time": "2024-09-09 22:15:36",
+    "link": "https://www.yuque.com/xlu103/rvt9mr/am9x60rvhwzuqrbg"
+  },
+  {
+    "title": "《蛤蟆先生去看心理医生》 | 不要与他人共谋苛责自己 · 语雀",
+    "description": "不要与他人共谋，严厉苛责自己，没有任何批判会比自己批判...",
+    "url": "https://www.yuque.com/xlu103/rvt9mr/hmxsqkxlys",
+    "image": "https://cdn.nlark.com/yuque/0/2024/png/34813220/1722951512512-81f06bfa-d027-425f-bbe7-9ff045a78301.png",
+    "create_time": "2024-08-06 20:51:06",
+    "update_time": "2024-08-29 12:08:28",
+    "link": "https://www.yuque.com/xlu103/rvt9mr/hmxsqkxlys"
+  },
+  {
     "title": "《拒看新聞的生活藝術》 | 不要让自己的大脑去关注某种蠢事 · 语雀",
     "description": "一直看垃圾媒体等与训练大脑去关注某种蠢事。",
     "url": "https://www.yuque.com/xlu103/rvt9mr/ememf3criktbx23f",
@@ -25,5 +34,14 @@ const articleList = [
     "create_time": "2023-09-04 23:13:11",
     "update_time": "2024-08-19 13:50:32",
     "link": "https://www.yuque.com/xlu103/rvt9mr/ememf3criktbx23f"
+  },
+  {
+    "title": "《理解愤怒》｜掌握一项新武器“愤怒” · 语雀",
+    "description": "价值就像是金子，人际交往，其实就是在互相采矿。",
+    "url": "https://www.yuque.com/xlu103/rvt9mr/uamoesykgu655ltz",
+    "image": "https://cdn.nlark.com/yuque/0/2024/png/34813220/1725868987818-44db7970-c56a-461c-824b-ccc533467f62.png",
+    "create_time": "2024-09-02 16:28:34",
+    "update_time": "2024-09-09 16:03:11",
+    "link": "https://www.yuque.com/xlu103/rvt9mr/uamoesykgu655ltz"
   }
 ];
